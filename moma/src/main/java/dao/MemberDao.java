fab26d5fb164c464631ab09540c50b7c362daaba
82109 <82109@192.168.219.141>
2022-03-28 14:52:59 +0900

    sol test

--- moma/src/main/java/dao/MemberDao.java
+++ moma/src/main/java/dao/MemberDao.java
@@ -2,4 +2,9 @@ package dao;
 
 public class MemberDao {
 	private static MemberDao instance = new MemberDao();
+	private MemberDao() {
+	}
+	private static MemberDao getInstance() {
+		return instance;
+	}
 }
