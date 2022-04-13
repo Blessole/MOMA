9a196dcda778e337844d130637d8dd518f1ff1b4
carrothay <hazel.hykim@gmail.com>
2022-04-13 14:09:52 +0900

    13 April commit
    4/13

--- moma/src/main/java/service/board/BoardMain.java
+++ moma/src/main/java/service/board/BoardMain.java
@@ -12,10 +12,10 @@ import service.CommandProcess;
 public class BoardMain implements CommandProcess {
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("BoardMain 자바파일 지나감");
+		//System.out.println("BoardMain 자바파일 지나감");
 		BoardDao bd = BoardDao.getInstance();
 		
-		final int ROW_PER_PAGE = 4; // 한 페이지에 게시글 4개 씩
+		final int ROW_PER_PAGE = 6; // 한 페이지에 게시글 4개 씩
 		final int PAGE_PER_BLOCK = 5; // 한 블럭에 5페이지 씩
 		
 		String pageNum = request.getParameter("pageNum"); // 페이지 번호
@@ -45,7 +45,7 @@ public class BoardMain implements CommandProcess {
 		request.setAttribute("endPage", endPage);
 		request.setAttribute("PAGE_PER_BLOCK", PAGE_PER_BLOCK);
 		
-		System.out.println("BoardMain 자바파일 지나감2");
+		//System.out.println("BoardMain 자바파일 지나감2");
 		
 		return "boardMain";
 	}
